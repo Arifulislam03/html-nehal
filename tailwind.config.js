a07cb8de93141ae8206d2be2830de8ccc2e0fe28
Arifulislam03 <75440947+Arifulislam03@users.noreply.github.com>
2022-05-20 22:28:52 +0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   mode: 'jit',
-     content: [ 
-      './*.html',
-    ],
+  purge: ['./*.html'],
+  darkMode: false,
    corePlugins: {
     container: true 
   },
@@ -38,143 +37,145 @@ module.exports = {
        }, 
       colors: {
         
-      
-        "indigo-500": "#7d7c7d",
-        "black": "#000000",
-       
-        'yellow-100': '#ffcc00',
-        'yellow-200': '#f7dfce',
-        'yellow-300': '#f5f4f6',
-     
-        black: {
-          100: '#000000',
-          200: '#1d1d1f',
-          300: '#131313',
-          400: '#312d22',
-          500: '#75756f',
+        "primary-100": "#ff5041",
+        "primary-200": "#f3dfdb",
+        "primary-300": "#ff50416b",
+        "dark-100": "#000",
+        'secondary': '#6f6b80',
+        'secondary-100': '#0F172A',
+
+
         
-          
-         
-        },
+        
         
       },
-
      
-      backgroundImage: {
-        "curved-blue-gradient": "url(/imagenes/precios/curved-blue-gradient-bg.svg)",
-        "blue-cyan-gradient": "linear-gradient(90deg, #C84E89 0%, #F15F79 100%);",
-        "border-gradient": "linear-gradient(90deg, #C84E89 0%, #F15F79 100%);",
-      
-          },
 
 
       placeholderColor: theme => theme('colors'),
       placeholderColor: {
-        "coolGray-600":"#4B5563",
+        "coolGray-400":"#9CA3AF",
       },
+
       borderColor: theme => ({
         ...theme('colors'),
          DEFAULT: theme('colors.gray.300', 'currentColor'),
-        'coolGray-100': '#f5f4f6',
-        'yellow-300': '#FDE047',
-       
+        'primary-300': '#94A3B8',
+        'coolGray-300': '#D1D5DB',
+        'coolGray-400': '#D9D9D9',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+        "indigo-500":"#6366F1",
        }),
-       borderWidth: {
-        DEFAULT: '1px',
-        '0': '0',
-        '2': '2px',
-       '3': '3px',
-        '4': '4px',
-       '5': '5px',
-       '8': '8px',
-      },
-     
       borderRadius: {
         'none': '0',
         DEFAULT: '0',
         'sm': '0.25rem',
-        'md': '8px',
+        'md': '0.625rem',
         'lg': '1.25rem',
         'xl': '0'
       },
-      
-       fontFamily: { 
-        sans: ['"Inter", sans-serif'],  
-        display: ['"Inter", sans-serif'],
-        body: ['"Inter", sans-serif'],   
+     
+      fontFamily: {
+        sans: [
+          '"Red Hat Display",sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
       },
       fontSize: {
-        'xs': '13px',
+        'xs': '0.75rem',
         'sm': '.875rem',
         'tiny': '.875rem',
          'base': '1rem',
          'lg': '1.125rem',
-         'xl': '1.25rem',
-         '2xl': '28px',
+         'xl': '1.563rem',
+
+         '2xl': '1.5rem',
         '3xl': '2rem',
         '4xl': '2.5rem',
          '5xl': '3rem',
          '6xl': '3.5rem',
-        '7xl': '4rem',
-        '18': '18px',
+        '7xl': '3.75rem',
        },
       boxShadow: {
         base:' 0px 1px 3px 0px #0000001A',
+        sm: '0px 16px 20px 0px rgba(0, 0, 0, 0.059)',
+        md: '0px 20px 40px 0px rgba(0, 0, 0, 0.102)',
+        lg: '0px 10px 15px 0px #0000001A',
+         xl: '0 7px 14px 0 rgba(0, 0, 0, 0.2)',
         none: 'none',
-        
       },
       maxWidth: {
-        '62.2': '200px',
-        '317': '317px',
-        '350': '350px',
-        '50p': '50%',
-
-      
+        '247': '247px',
+        '950': '950px',
+        '1/2': '50%',
+        '3/4': '75%',
+        '3/4': '75%',
       },
       width: {
         '15': '3.75rem',
-        '168': '42rem',
-        '87.5': '350px',
-        '38.5': '154px',
-        
-        
-       
+      
       },
       height: {
-        '15': '3.75rem',
-        '35': '35px',
-       
+        544: '34rem',
+      
        },
+
+       backgroundImage: {
+        "blue-cyan-gradient": "linear-gradient(90deg,rgba(20,20,51,.9) 0,#141433 45%,#141433 100%)",
+        "border-gradient": "linear-gradient(90deg, #C84E89 0%, #F15F79 100%);",
+      
+          },
        scale: {
         '0': '0',
        '25': '.25',
+        '50': '.5',
+        '75': '.75',
+        '90': '.9',
+       '95': '.95',
+        '100': '1',
+       '105': '1.05',
+       '110': '1.1',
+        '125': '1.25',
+        '150': '1.5',
+       '200': '2',
       },
       zIndex: {
         '-9999': '-9999',
         '0': 0,
         '1': -1,
         '2': 2,
+       '999': 999,
+       '9999': 9999,
         'auto': 'auto',
       },
       inset: {
-        '45': "180px",
-        
-      },
-      spacing: {
-        '25': '100px',
+        '10.5': "10.5%",
+        '7%': "7%",
+        '5%': "5%",
+
       },
       opacity: {
-        '60': '60%',
-        '3': '0.6',
-        
+        '7': '.7',
+        '0.9': '0.9',
+        '1': '1',
        },
        lineHeight: {
         '0': '0',
-        '13': '13px',
-        '3.25': '3.25',
-        '1.3': '1.3',
-       
- 
+        '7': '1.8rem',
+
        }
     },
   },
